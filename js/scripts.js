3b2dbf794087d1a8e7b31bb29282ab175f64e9c1
Grzegorz Pawlak <kalejdoskopwroclaw@gmail.com>
2017-09-09 12:35:07 +0200

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // scripts.js
 
+// coment 4
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+alert('Hello, ' + name); // coment allert
+console.log('Hello, ' + name); 
+/* 
+comment 2 
+comment 3
+*/
