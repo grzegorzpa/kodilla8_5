8b79a54458460f60a5da5198a3a891482bc0ee5b
Grzegorz Pawlak <kalejdoskopwroclaw@gmail.com>
2017-09-11 18:12:54 +0200

    add correct

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 var	a = prompt('Dłguść boku:','10')
 	h = prompt('Podaj wysokość:', '12')
 	triangleArea = a * h /2
-document.getElementById("prompt").innerHTML =
-"Pole Twojego Trójkąta wynosi: " + triangleArea;
+document.getElementById("prompt").innerHTML = "Pole Twojego Trójkąta wynosi: " + triangleArea;
 alert('Twój wynik to ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
